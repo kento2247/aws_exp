b6b02592849dd388311b68e9ac1a3698a79ff95d
KentoTokura <tkento1985@gmail.com>
2023-11-02 11:23:36 +0900

    add hostNode

---
+++ hostNode/index.js
@@ -0,0 +1,17 @@
+const AWS = import("aws-sdk");
+const lambda = new AWS.Lambda();
+
+const params = {
+  FunctionName: "test-py",
+  InvocationType: "RequestResponse", // 同期呼び出し
+  Payload: JSON.stringify({ key: "value" }), // 関数Bに渡すデータ (JSON形式)
+};
+
+lambda.invoke(params, function (err, data) {
+  if (err) {
+    console.error(err, err.stack);
+  } else {
+    const result = JSON.parse(data.Payload);
+    console.log(result);
+  }
+});
