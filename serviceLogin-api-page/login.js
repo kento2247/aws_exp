b5c950463466cf50853836c12c8d674a9a8f4e7f
KentoTokura <tkento1985@gmail.com>
2023-11-05 21:26:12 +0900

    complete login

--- serviceLogin-api-page/login.js
+++ serviceLogin-api-page/login.js
@@ -1,3 +1,9 @@
+const service_login_api =
+  "https://5sbpzbfrgl.execute-api.us-east-1.amazonaws.com/api";
+let redirect_url = new URLSearchParams(window.location.search).get(
+  "redirect_url"
+);
+
 document
   .getElementById("loginForm")
   .addEventListener("submit", function (event) {
@@ -11,7 +17,7 @@ document
       password: password,
     };
 
-    fetch("https://example.com/login-endpoint", {
+    fetch(service_login_api, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -20,8 +26,11 @@ document
     })
       .then((response) => response.json())
       .then((data) => {
-        if (data.success) {
-          window.location.href = "https://example.com/success-page";
+        console.log("login api result: ", data);
+        let response_body = JSON.parse(data.body);
+        if (response_body.success) {
+          window.location.href =
+            redirect_url + "?service_id=" + response_body.user_id;
         } else {
           document.getElementById("flashMessage").innerText =
             "Login failed. Please try again.";
